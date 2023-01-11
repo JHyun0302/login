6f39aa7306f97b3c9f10edbfd8c0a88b3bb843a4
jaehyun <wogustkfkd11@gmail.com>
2023-01-11 10:39:16 +0900

    config - ArgumentResolver 추가

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,18 +1,31 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
 import hello.login.web.interceptor.LoginCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /**
+     * argumentResolver 작동을 위해 등록
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
     /**
      * 스프링 인터셉터 작동을 위해 등록
      */
@@ -47,10 +60,13 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    //    @Bean //LoginCheckFilter 작동을 위해 필요
+    @Autowired
+    LoginCheckFilter loginCheckFilter; //스프링 빈으로 의존관계 주입 형식
+
+    @Bean //LoginCheckFilter 작동을 위해 필요
     public FilterRegistrationBean LoginCheckFilter() { //스프링 부트를 사용해 필터 등록하는 방법
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LoginCheckFilter()); //우리가 만든 LogFilter.java
+        filterRegistrationBean.setFilter(loginCheckFilter); //우리가 만든 LoginCheckFilter.java
         filterRegistrationBean.setOrder(2); //필터 순서(체인 형식이니까)
 
         filterRegistrationBean.addUrlPatterns("/*"); //모든 url에 적용
